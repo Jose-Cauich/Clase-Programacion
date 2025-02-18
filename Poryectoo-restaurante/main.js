93a86297323cd435219f16e5fca0634950634449
CauichDev <cauichdev@gmail.com>
2025-02-18 07:08:55 -0600

    commit-prefinal

--- Poryectoo-restaurante/main.js
+++ Poryectoo-restaurante/main.js
@@ -1,10 +1,20 @@
 var swiper = new Swiper(".home", {
     spaceBetween: 30,
     centeredSlides: true,
-
     
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
